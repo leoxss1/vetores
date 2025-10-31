a39333991b6ca4918061a2a48d8daca8a01cc2c2
Leoxss <leo2007ssll@gmail.com>
2025-10-30 22:27:52 -0300

    Remove commented author name from Ex3RANDOM.java

--- Ex3RANDOM.java
+++ Ex3RANDOM.java
@@ -11,23 +11,20 @@ public class Ex3RANDOM {
             numeros[i] = 10 - leitor.nextInt(10); 
         }
 
-        
         menor = numeros[0];
 
-        
         for (int i = 1; i < 10; i++) {
             if (numeros[i] < menor) {
                 menor = numeros[i];
             }
         }
 
-        
         System.out.println("Números sorteados:");
         for (int i = 0; i < 10; i++) {
             System.out.println(numeros[i]);
         }
 
-        
         System.out.println("O menor número é: " + menor);
     }
 }
+
