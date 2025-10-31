626c8d9e1c7edb7e021a6e3f8db0ad31bed5c8ca
Leoxss <leo2007ssll@gmail.com>
2025-10-30 22:27:10 -0300

    Add files via upload

---
+++ Ex3RANDOM.java
@@ -0,0 +1,33 @@
+//Leonardo xavier santana santos
+import java.util.Random;
+public class Ex3RANDOM {
+    public static void main(String[] args) {
+        Random leitor = new Random();
+        int[] numeros = new int[10];
+        int menor;
+
+       
+        for (int i = 0; i < 10; i++) {
+            numeros[i] = 10 - leitor.nextInt(10); 
+        }
+
+        
+        menor = numeros[0];
+
+        
+        for (int i = 1; i < 10; i++) {
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+        }
+
+        
+        System.out.println("Números sorteados:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numeros[i]);
+        }
+
+        
+        System.out.println("O menor número é: " + menor);
+    }
+}
